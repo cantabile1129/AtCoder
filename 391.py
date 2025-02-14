c30c62a1610240eddac04a40c9b9dd9b63cd6417
cantabile1129 <kanta.fukao@keio.jp>
2025-02-14 12:25:09 +0900

    第1問自分の正しい解答

--- 391.py
+++ 391.py
@@ -1,2 +1,8 @@
-print("Hello, World")
-print("Can you see it?")
+D = [["E", "W"], ["N", "S"], ["NE", "SW"], ["NW", "SE"]]
+C = input()
+for i in range(4):
+    if (C == D[i][0]):
+        print(D[i][1])
+    elif (C == D[i][1]):
+        print(D[i][0])
+        
