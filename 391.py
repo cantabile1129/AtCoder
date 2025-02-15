8126e75673f135d2ef075b0b133c5cb5346bf4bb
cantabile1129 <kanta.fukao@keio.jp>
2025-02-15 12:34:16 +0900

    Cのanswerまで

--- 391.py
+++ 391.py
@@ -53,4 +53,30 @@ for a in range(n - m + 1):
 """
 
 
-#B answer
+#C answer
+N, Q = map(int, input().split())
+ans = 0
+cnt = [0] + [1] * N
+pos = list(range(N + 1))
+ans_list = []
+for i in range(Q):
+    query = list(map(int, input().split()))
+    if query[0] == 1:
+        P, H = query[1:]
+        # 鳩 P は移動する前は巣 pos[P] にいる
+        # 鳩 P が移動することで 巣 pos[P] にいる鳩の数が 2 から 1 に減る場合, ans を 1 減らす
+        if cnt[pos[P]] == 2:
+            ans -= 1
+        # 巣 pos[P] の鳩の数を 1 減らす
+        cnt[pos[P]] -= 1
+
+        # 鳩 P がいる巣を H に変更する
+        pos[P] = H
+        # 巣 H の鳩の数を 1 増やす
+        cnt[pos[P]] += 1
+        # 鳩 P が移動することで 巣 H にいる鳩の数が 1 から 2 に増える場合，ans を 1 増やす
+        if cnt[pos[P]] == 2:
+            ans += 1
+    else:
+        ans_list.append(ans)
+print(ans_list)
